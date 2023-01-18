7d30bd13f0afa9bdafee74c1c2fd1a507e898262
sa8a <jpkrllll@yahoo.co.jp>
2023-01-18 18:37:55 +0900

    画面遷移のサンプルを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hashtag_app/first_page.dart';
+import 'package:hashtag_app/second_page.dart';
+import 'package:hashtag_app/third_page.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final router = GoRouter(
+    initialLocation: '/first',
+    routes: [
+      GoRoute(
+        path: '/first',
+        builder: (context, state) => const FirstPage(),
+      ),
+      GoRoute(
+        path: '/second',
+        builder: (context, state) => const SecondPage(),
+      ),
+      GoRoute(
+        path: '/third',
+        builder: (context, state) => const ThirdPage(),
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+    return MaterialApp.router(
+      routeInformationProvider: router.routeInformationProvider,
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
     );
   }
 }
